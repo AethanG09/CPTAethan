c057b23e3bcfa52bf30d95d83ac4dadceca9e272
Aethan <aethan.guiao27@ycdsbk12.ca>
2025-05-23 09:38:11 -0400

    adadada

---
+++ CPTAethan.java
@@ -0,0 +1,8 @@
+import arc.*;
+
+public class CPTAethan{
+	public static void main(String[] args){
+		Console con = new Console();
+		con.println("skibidi");
+	}
+}
---
+++ CPTAethan1.java
@@ -0,0 +1,7 @@
+import arc.*;
+
+public class CPTAethan1{
+	public static void main(String[] args){
+		Console con = new Console();
+	}
+}
